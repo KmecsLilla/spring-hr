b59a7ac642343c5f55b6db9db027a3418438461c
KmecsLilla <kmecs.lilla@gmail.com>
2021-05-17 08:06:24 +0200

    Implement query for companies with employees fit to given conditions

--- src/main/java/hu/webuni/hr/lilla/repository/CompanyRepository.java
+++ src/main/java/hu/webuni/hr/lilla/repository/CompanyRepository.java
@@ -1,8 +1,18 @@
 package hu.webuni.hr.lilla.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import hu.webuni.hr.lilla.model.Company;
+import hu.webuni.hr.lilla.model.Employee;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+	@Query("SELECT c FROM Company c JOIN c.employees e WHERE e.salary > :minSalary" )
+	public List<Employee> findEmployeeWithSalaryHigherThan(int minSalary);
+
+	@Query("SELECT c FROM Company c WHERE Size(c.employees) > :minEmployeeCount")
+	public List<Employee> findEmployeeCountHigherThan(int minEmployeeCount);
+
 }
